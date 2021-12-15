2a862a501bf6621842bbc113a351a5b307bdee91
AntonDubiaha <96083195+AntonDubiaha@users.noreply.github.com>
2021-12-15 17:12:01 +0200

    New functions

--- viceversa/viceversa/views.py
+++ viceversa/viceversa/views.py
@@ -8,4 +8,4 @@ def home(request):
 def reverse(request):
     user_text = request.GET['usertext']
     reversed_text = user_text[::-1]
-    return render(request, 'reverse.html', {'usertext': user_text}, {'reversedtext': reversed_text})
+    return render(request, 'reverse.html', {'usertext': user_text, 'reversedtext': reversed_text})
