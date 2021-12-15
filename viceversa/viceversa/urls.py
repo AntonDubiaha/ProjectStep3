71eb600af81521db142d7b63b53fedaea50bfa1a
AntonDubiaha <96083195+AntonDubiaha@users.noreply.github.com>
2021-12-15 16:28:27 +0200

    New functions

--- viceversa/viceversa/urls.py
+++ viceversa/viceversa/urls.py
@@ -13,9 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.urls import re_path
+from django.urls import path
 from . import views
 
 urlpatterns = [
-    re_path('', views.home),
+    path('', views.home),
+    path('reverse/', views.reverse),
 ]
